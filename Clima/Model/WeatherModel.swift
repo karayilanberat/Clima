af64ffb6fa903fb8afe6248e33540f3da8046db8
karayilanberat <125715212+karayilanberat@users.noreply.github.com>
2024-07-20 16:39:03 +0300

    Update WeatherModel.swift

--- Clima/Model/WeatherModel.swift
+++ Clima/Model/WeatherModel.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+struct WeatherModel {
+    let conditionId: Int
+    let cityName: String
+    let temp: Double
+    
+    var tempStr: String {
+        return String(format: "%.0f", temp-273.15)
+    }
+    
+    var conditionName: String {
+        switch conditionId {
+        case 200...232:
+            return "cloud.bolt"
+        case 300...321:
+            return "cloud.drizzle"
+        case 500...531:
+            return "cloud.rain"
+        case 600...622:
+            return "cloud.snow"
+        case 701...781:
+            return "cloud.fog"
+        case 800:
+            return "sun.max"
+        case 801...804:
+            return "cloud.bolt"
+        default:
+            return "cloud"
+        }
+    }
+}
